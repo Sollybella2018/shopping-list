62b7c36340121a38df4685a454f640cf29404153
Sollybella2018 <assilomar@hotmail.com>
2025-03-17 10:51:58 +0300

    new

--- script.js
+++ script.js
@@ -192,6 +192,44 @@ function init() {
 
 init();
 
+//const intervallId = setInterval(myCallback, 1000,'Hello');
+
+//function myCallback(a){
+    //console.log(a,Date.now());
+
+//};
+
+let intervalId ;
+function startChange (){
+    if(!intervalId){
+        intervalId= setInterval(changeRandomColor,1000);
+
+    }
+}
+
+//function changeColor(){
+   // if (document.body.style.backgroundColor !== 'black'){
+       // document.body.style.backgroundColor = 'black';
+       // document.body.style.color = 'white';
+    //}else {
+      //  document.body.style.backgroundColor = 'white';
+       // document.body.style.color = 'black';
+    //}
+
+//}
+
+function changeRandomColor(){
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+
+}
+
+function stopChange(){
+    clearInterval(intervalId);
+}
+
+document.getElementById('stop').addEventListener('click',stopChange);
+document.getElementById('start').addEventListener('click', startChange);
 
 
 //setTimeout(changeText, 2000);
